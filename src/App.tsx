fd4fdeffb08549b35772cf2f069040f855937c21
Егорова Мария Александровна <maleksandrovnegorova@sberbank.ru>
2023-11-04 12:33:23 +0300

    feat: base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='block1' className='block block1'>Block1</div>
+      <div id='block2' className='block block2'>Block2</div>
+      <button id='start-button' className='button'>Start</button>
     </div>
   );
 }
